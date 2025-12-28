728bde0039af817632ecdff7f48a49fcc95cf5d7
Hitesh Mane <HiteshMane5hm@gmail.com>
2025-12-28 16:22:23 +0530

    Shopping Application 1 Created Enities.

---
+++ ShoppingApplication1/src/Customer.java
@@ -0,0 +1,49 @@
+
+public class Customer {
+
+	private int customerId;
+	private String customername;
+	private Address custoAddress;
+	private Product customerproduct;
+	
+	public Customer(int customerId, String customername, Address custoAddress, Product customerproduct) {
+		super();
+		this.customerId = customerId;
+		this.customername = customername;
+		this.custoAddress = custoAddress;
+		this.customerproduct = customerproduct;
+	}
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomername() {
+		return customername;
+	}
+
+	public void setCustomername(String customername) {
+		this.customername = customername;
+	}
+
+	public Address getCustoAddress() {
+		return custoAddress;
+	}
+
+	public void setCustoAddress(Address custoAddress) {
+		this.custoAddress = custoAddress;
+	}
+
+	public Product getCustomerproduct() {
+		return customerproduct;
+	}
+
+	public void setCustomerproduct(Product customerproduct) {
+		this.customerproduct = customerproduct;
+	}
+
+}
---
+++ ShoppingApplication1/src/Product.java
@@ -0,0 +1,43 @@
+
+public class Product {
+	
+	private int productId;
+	private String productname;
+	private double productprice;
+	private int productquantity;
+	
+	public Product(int productId, String productname, double productprice, int productquantity) {
+		super();
+		this.productId = productId;
+		this.productname = productname;
+		this.productprice = productprice;
+		this.productquantity = productquantity;
+	}
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public double getProductprice() {
+		return productprice;
+	}
+	public void setProductprice(double productprice) {
+		this.productprice = productprice;
+	}
+	public int getProductquantity() {
+		return productquantity;
+	}
+	public void setProductquantity(int productquantity) {
+		this.productquantity = productquantity;
+	}
+	
+
+}
