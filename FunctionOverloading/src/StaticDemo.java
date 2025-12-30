132e7108630f8a9b5e1882a51b3cbd4ebb0181d8
Hitesh Mane <HiteshMane5hm@gmail.com>
2025-12-30 18:37:25 +0530

    Static import added.

--- FunctionOverloading/src/StaticDemo.java
+++ FunctionOverloading/src/StaticDemo.java
@@ -1,16 +1,17 @@
+import static java.lang.System.out; 
 class StaticDemo {
     // Static variable
     static int count = 0;
     
     // Static block
     static {
-        System.out.println("Static block executed.");
+        out.println("Static block executed.");
         count = 10;
     }
     
     // Static method
     static void displayCount() {
-        System.out.println("Count: " + count);
+        out.println("Count: " + count);
     }
     
     // Non-static method (for contrast)
